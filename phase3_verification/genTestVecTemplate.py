0b63ad232c71f6bf87f34f0499f296514e14e023
dt27182 <dt27182@berkeley.edu>
2012-11-14 23:01:11 -0800

    modift phase 3 python testbench generator

--- phase3_verification/genTestVecTemplate.py
+++ phase3_verification/genTestVecTemplate.py
@@ -16,10 +16,10 @@ tcurrent = 0
 fi = open('func_check.vec', 'w')
 
 # Lists of inputs and outputs and corresponding bit length
-inputs = ['data', 'data_b', 'en', 'write', 'InA', 'InB', 'InC', 'InD', 'ax', 'bx', 'cx', 'cin', 'asout', 'bsout', 'csout', 'dsout']
-inputSize = [16, 16, 1, 1, 6, 6, 6, 6, 1, 1, 1, 1, 2, 2, 2, 2]
-outputs = ['out', 'out_exp', 'cout', 'cout_exp']
-outputSize = [8, 8, 1, 1]
+inputs = ['a_data', 'a_data_b', 'b_data', 'b_data_b','c_data', 'c_data_b','d_data', 'd_data_b','en', 'write', 'ina', 'inb', 'inc', 'ind', 'ax', 'bx', 'cx', 'cin', 'asout', 'bsout', 'csout', 'dsout', 'out_exp', 'cout_exp']
+inputSize = [4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 6, 6, 6, 6, 1, 1, 1, 1, 2, 2, 2, 2, 8, 1]
+outputs = ['out', 'cout']
+outputSize = [8, 1]
 
 
 # Write file header
@@ -57,8 +57,34 @@ fi.write('\n')
 # Write here the rest of the header
 ###################################
 
-# ----------------------------------
+fi.write('io ')
+for idx, inp in enumerate(inputs):
+	fi.write('i'*inputSize[idx]+' ')
+for idx, outp in enumerate(outputs):
+	fi.write('o'*outputSize[idx]+' ')
+fi.write('\n')
 
+fi.write('tunit ns')
+fi.write('\n')
+
+fi.write('tdelay 2 ')
+for idx, inp in enumerate(inputs):
+	fi.write('0'*inputSize[idx]+' ')
+for idx, outp in enumerate(outputs):
+	fi.write('1'*outputSize[idx]+' ')
+fi.write('\n')
+
+fi.write('slope 1.2')
+fi.write('\n')
+fi.write('vih 1.2')
+fi.write('\n')
+fi.write('vil 0.0')
+fi.write('\n')
+fi.write('vth 0.6')
+fi.write('\n')
+fi.write('vil 0.0')
+fi.write('\n')
+fi.write('\n')
 ####################################
 # Now write the actual input vectors
 ####################################
@@ -80,10 +106,44 @@ for i in range(0, 2**LSB_bits):
    	t_string = str(tcurrent).zfill(4)
    	t_string = '%s.0'%(t_string)
 	fi.write(t_string+' ')
+
+	# Convert SRAM address into a binary string to be printed in the file
+	inpLSB = bin(i)[2:].zfill(4)
+	# Compute LUT SRAM write values
+	LUT_SRAM_values = []
+	for j in range(0, 2**MSB_bits):
+		inpMSB = bin(j)[2:].zfill(4)
+		LUT_SRAM_values.append(str(int((int(inpLSB[0]) or int(inpLSB[1]) or int(inpLSB[2]) or int(inpLSB[3])) and ((not int(inpLSB[0])) or (not int(inpLSB[1])) or (not int(inpLSB[2])) or (not int(inpLSB[3]))))))
 	# Assign 0 to the en signal (pre-charge) and don't care to all other signals
 	for idx, inp in enumerate(inputs):
 		if inp == 'en':
 			fi.write('0'*inputSize[idx]+' ')
+		elif inp == 'ina' or inp == 'inb' or inp == 'inc' or inp == 'ind':
+			fi.write(inpLSB + ' ')
+		elif inp == 'a_data':
+			fi.write(LUT_SRAM_values[0])
+			fi.write(LUT_SRAM_values[1])
+			fi.write(LUT_SRAM_values[2])
+			fi.write(LUT_SRAM_values[3])
+			fi.write(' ')
+		elif inp == 'b_data':
+			fi.write(LUT_SRAM_values[4])
+			fi.write(LUT_SRAM_values[5])
+			fi.write(LUT_SRAM_values[6])
+			fi.write(LUT_SRAM_values[7])
+			fi.write(' ')
+		elif inp == 'c_data':
+			fi.write(LUT_SRAM_values[8])
+			fi.write(LUT_SRAM_values[9])
+			fi.write(LUT_SRAM_values[10])
+			fi.write(LUT_SRAM_values[11])
+			fi.write(' ')
+		elif inp == 'd_data':
+			fi.write(LUT_SRAM_values[12])
+			fi.write(LUT_SRAM_values[13])
+			fi.write(LUT_SRAM_values[14])
+			fi.write(LUT_SRAM_values[15])
+			fi.write(' ')
 		else:
 			fi.write('x'*inputSize[idx]+' ')
 	for idx, outp in enumerate(outputs):
@@ -91,8 +151,7 @@ for i in range(0, 2**LSB_bits):
 	fi.write('\n')
 	tcurrent = tcurrent + tstep
 
-	# Convert SRAM address into a binary string to be printed in the file
-	inpLSB = bin(i)[2:].zfill(4)
+	
 	
 	#############################
 	# Now you need to create strings to set data and data_b, compute F and activate the correct cell in the LUT to write into
