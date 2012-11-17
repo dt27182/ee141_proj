5e52e937812c9fc1114785ba835ada3d235b65de
Xiao_Chen <ee141-aj@t7400-7.EECS.Berkeley.EDU>
2012-11-17 11:21:46 -0800

    adding python part 3 and 4

--- phase3_verification/genTestVecTemplate.py
+++ phase3_verification/genTestVecTemplate.py
@@ -188,8 +188,7 @@ for i in range(0, 2**LSB_bits):
 		fi.write('x'*outputSize[idx]+' ')
 	fi.write('\n')
 	tcurrent = tcurrent + tstep
-
-# 2a. Read 8 LUT output
+"""# 2a. Read 8 LUT output
 for i in range(0, 2**LSB_bits):
 	inpLSB = bin(i)[2:].zfill(4)
 	#Precharge
@@ -314,7 +313,7 @@ for i in range(0, 2**LSB_bits):
 		fi.write('x'*outputSize[idx]+' ')
 	fi.write('\n')
 	tcurrent = tcurrent + tstep
-"""
+
 	#############################
 	# Now you need to create strings to set data and data_b, compute F and activate the correct cell in the LUT to write into
 	#############################
